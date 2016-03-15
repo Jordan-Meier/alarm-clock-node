891bbe06a6fb3746c630a58689c499fc3bd6399a
Jordan Meier and Mary Warrington <jbirdmeier@gmail.com>
2016-03-15 08:41:01 -0700

    install vinyl source stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/*-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
